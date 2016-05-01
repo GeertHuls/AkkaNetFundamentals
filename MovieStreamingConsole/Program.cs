1bf4b97b44a2494ac1749dfa73cff8e822e6e0b2
Geert Huls <geert.huls82@gmail.com>
2016-05-01 22:12:52 +0200

    Minimal actor system setup

---
+++ MovieStreamingConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Akka.Actor;
+
+namespace MovieStreamingConsole
+{
+    class Program
+    {
+        private static ActorSystem _movieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            _movieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+
+            Console.ReadLine();
+
+            _movieStreamingActorSystem.Terminate();
+        }
+    }
+}
