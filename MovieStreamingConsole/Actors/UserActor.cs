afc110cc0958faa32e23c5d8101913bb6edb6428
Geert Huls <geert.huls82@gmail.com>
2016-05-02 22:21:06 +0200

    User actor behavior using if statements

--- MovieStreamingConsole/Actors/UserActor.cs
+++ MovieStreamingConsole/Actors/UserActor.cs
@@ -1,10 +1,60 @@
 ﻿using System;
 using Akka.Actor;
+using MovieStreamingConsole.Messages;
 
 namespace MovieStreamingConsole.Actors
 {
     public class UserActor : ReceiveActor
     {
+        private string _currentlyWatching;
+
+        public UserActor()
+        {
+            Console.WriteLine("Creating a UserActor");
+
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message));
+            Receive<StopMovieMessage>(message => HandleStopMovieMessage(message));
+
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            if (_currentlyWatching != null)
+            {
+                Console.WriteLine("Error: cannot start playing another movie before stopping the existing one");
+            }
+            else
+            {
+                StartPlayingMovie(message.MovieTitle);
+            }
+        }
+
+        private void StartPlayingMovie(string title)
+        {
+            _currentlyWatching = title;
+
+            Console.WriteLine("User is currently watching '{0}'", title);
+        }
+
+        private void HandleStopMovieMessage(StopMovieMessage message)
+        {
+            if (_currentlyWatching == null)
+            {
+                Console.WriteLine("Error: the movie cannot be stopped, nothing is playing");
+            }
+            else
+            {
+                StopPlayingCurrentMovie();
+            }
+        }
+
+        private void StopPlayingCurrentMovie()
+        {
+            Console.WriteLine("User has stopped watching  '{0}'", _currentlyWatching);
+
+            _currentlyWatching = null;
+        }
+
         protected override void PreStart()
         {
             Console.WriteLine("UserActor PreStart");
