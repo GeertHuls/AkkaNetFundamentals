7f21869ba5830392e59b1a3c66c3ce3398fe28c2
Geert Huls <geert.huls82@gmail.com>
2016-05-04 22:35:17 +0200

    Created remote movie streaming actor system

---
+++ MovieStream.Remote/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using MovieStreamingConsole;
+
+namespace MovieStream.Remote
+{
+    internal class Program
+    {
+        private static ActorSystem _movieStreamActorSystem;
+
+        private static void Main(string[] args)
+        {
+            ColorConsole.WriteLineGray("Creating MovieStreamingActorSystem in remote process");
+
+            Start().Wait();
+
+            Console.ReadLine();
+        }
+
+        private static async Task Start()
+        {
+            CreateActorSystem();
+            await TerminateActorSystem();
+        }
+
+        private static void CreateActorSystem()
+        {
+            _movieStreamActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+        }
+
+        private static async Task TerminateActorSystem()
+        {
+            await _movieStreamActorSystem.Terminate();
+        }
+    }
+}
