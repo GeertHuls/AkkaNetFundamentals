b2d34223ffcdc652ade05b15102565095596dcbc
Geert Huls <geert.huls82@gmail.com>
2016-05-16 22:08:57 +0200

    Code cleanup remote actor

--- MovieStream.Remote/Program.cs
+++ MovieStream.Remote/Program.cs
@@ -1,42 +1,17 @@
-﻿using System;
-using System.Threading.Tasks;
-using Akka.Actor;
+﻿using Akka.Actor;
 using MovieStreaming.Common;
 
 namespace MovieStream.Remote
 {
     internal class Program
     {
-        private static ActorSystem _movieStreamActorSystem;
-
-        private static void Main(string[] args)
+        private static void Main()
         {
             ColorConsole.WriteLineGray("Creating MovieStreamingActorSystem in remote process");
 
-            Start().Wait();
-
-            Console.ReadLine();
-        }
-
-        private static async Task Start()
-        {
-            CreateActorSystem();
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            ColorConsole.WriteLineYellow("Press any key to shutdown remote actor system.");
-            Console.ReadLine();
-
-            await TerminateActorSystem();
-        }
-
-        private static void CreateActorSystem()
-        {
-            _movieStreamActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
-        }
-
-        private static async Task TerminateActorSystem()
-        {
-            await _movieStreamActorSystem.Terminate();
+            ActorSystem.Create("MovieStreamingActorSystem")
+                .WhenTerminated
+                .Wait();
         }
     }
 }
